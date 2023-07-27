fb6e45517a7152d0401cb9c0977ceb994f9689b3
Abhishek Shrestha <np01cp4a210122@islingtoncollege.edu.np>
2023-07-27 13:13:47 +0545

    Restored Server.js code

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,33 @@
 
+require('dotenv').config()
+
+//importing the express package
+const express = require("express");
+const mongoose = require("mongoose")
+
+const workoutRoutes = require("./routes/workouts.routes");
+
+//creating express app
+const app = express();
+
+//middleware to parse incoming requests with JSON payloads
+app.use(express.json())
+
+//middleware to log out request method and path
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+//connect to the db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //Listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log(`Listening on port ${process.env.PORT}`)
+        })
+    })
+    .catch((error) => console.log(error))
+
+//routes
+app.use('/api/workouts', workoutRoutes)
